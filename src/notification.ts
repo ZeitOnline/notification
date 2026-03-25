f8f77fb3446cfaadfca7df48130fbb6f3a39e8b6
Daniel Kreitschmann <daniel.kreitschmann@zeit.de>
2026-03-25 23:11:48 +0100

    refactor: fix prettier issue
    Ref: ENG-718

--- src/notification.ts
+++ src/notification.ts
@@ -167,11 +167,13 @@ export class Notification {
 		notification.setAttribute('role', 'alert');
 		notification.setAttribute('aria-live', 'assertive');
 
-		notification.innerHTML = this.getSvgIcon(icon)
-			+ (message ? `<span class="z-notification__message">${message}</span>` : '')
-			+ (link ? `<a href="${link.href}" class="z-notification__action-btn" role="link">${link.text}</a>` : '')
-			+ (!link && button ? `<button class="z-notification__action-btn" role="button">${button.text}</button>` : '')
-			+ CLOSE_BUTTON_HTML;
+		// prettier-ignore-start
+		notification.innerHTML = this.getSvgIcon(icon) +
+			(message ? `<span class="z-notification__message">${message}</span>` : '') +
+			(link ? `<a href="${link.href}" class="z-notification__action-btn" role="link">${link.text}</a>` : '') +
+			(!link && button ? `<button class="z-notification__action-btn" role="button">${button.text}</button>` : '') +
+			CLOSE_BUTTON_HTML;
+		// prettier-ignore-end
 
 		(this.container as HTMLElement).appendChild(notification);
 
