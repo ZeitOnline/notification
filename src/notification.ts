5d9111455689112a1cf9b61abdad4b3b4ff5e61f
Daniel Kreitschmann <daniel.kreitschmann@zeit.de>
2026-03-25 23:02:04 +0100

    refactor: use original width and height in close button svg and remove custom styles for error notifications
    Refs: ENG-718

--- src/notification.ts
+++ src/notification.ts
@@ -20,9 +20,9 @@ const CLOSE_BUTTON_HTML = `
 		<svg class="z-notification__close-ring" viewBox="0 0 24 24" aria-hidden="true">
 			<circle cx="12" cy="12" r="11.5"/>
 		</svg>
-		<svg class="z-notification__close-cross" width="12" height="12" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-			<path d="M15 15L2.99999 3.00001" stroke="currentColor" stroke-width="1.5"/>
-			<path d="M15 3.00001L3.00001 15" stroke="currentColor" stroke-width="1.5"/>
+		<svg class="z-notification__close-cross" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+			<path d="M15 15L3 3" stroke="currentColor" stroke-width="1.5"/>
+			<path d="M15 3L3 15" stroke="currentColor" stroke-width="1.5"/>
 		</svg>
 	</button>`;
 
